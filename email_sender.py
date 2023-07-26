4dba511a4c03262091e5a441471b83006f1bc8d3
Mehmet Ali Soylu <mhmtsoylu1928@gmail.com>
2023-07-26 13:52:03 +0300

    Create second frame for login & logout

--- email_sender.py
+++ email_sender.py
@@ -24,5 +24,16 @@ password_entry = Entry(f1, show="*")
 email_entry.grid(row=1, column=1, pady=5)
 password_entry.grid(row=2, column=1)
 
-button1 = Button(f1, text="Login", width=10, bg="black", fg="white", command= lambda : login())
+button1 = Button(f1, text="Login", width=10, bg="black", fg="white", command=lambda: login())
 button1.grid(row=3, columnspan=3, pady=10)
+
+
+f2 = Frame(root)
+f2.pack(side=TOP, expand=NO, fill=NONE)
+
+label4 = Label(f2, width=20, bg="gray", fg="red", text="Login success", font="Arial 12 bold")
+label4.grid(row=0, column=0, columnspan=2, pady=5)
+
+button2 = Button(f2, text="Logout", bg="black", fg="white", command=lambda: logout())
+button2.grid(row=0, column=4, sticky=E, pady=10, padx=(5,0))
+
